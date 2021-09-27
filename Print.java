a202bccbf18157cbc799207852948b37c5048258
Sahrul Ismail Usman <79994875+arulismail7@users.noreply.github.com>
2021-09-27 11:17:50 +0700

    No 1

---
+++ Print.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.sesi.pkg1;
+
+/**
+ *
+ * @author ASUS
+ */
+import javax.swing.JOptionPane;
+public class Print {
+    public static void main(String[] args) {
+        
+  
+        JOptionPane.showInternalMessageDialog(null,"Belajar Java" +
+                " Sangat Mudah Sekali" +
+                " dan sangat menyenangkan");
+    }
+    
+}

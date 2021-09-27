7349dce1c4652e06395d2983a2a05a787581cc25
Sahrul Ismail Usman <79994875+arulismail7@users.noreply.github.com>
2021-09-27 11:15:27 +0700

    Add files via upload

---
+++ ProgramJavaSesi1.java
@@ -0,0 +1,20 @@
+
+package program.java.sesi.pkg1;
+
+/**
+ *
+ * @author ASUS
+ */
+import javax.swing.*;
+public class ProgramJavaSesi1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String belajarJava = JOptionPane.showInputDialog("Anda Sedang Belajar Apa ?");
+        JOptionPane.showMessageDialog(parentComponent null, message "Belajar "+belajarJava+"sangat mudah");
+        // TODO code application logic here
+    }
+    
+}
